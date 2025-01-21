a244d24808936b002879d90c0e5ded2188496a06
Wendel Maciel <wendelmcl2@gmail.com>
2025-01-21 19:42:47 -0300

    Retirada de arquivos do Repositório
    Retirada de arquivos do Repositório

--- Unicatólica/Semestre_2024_1/Computação e Algoritmos - Python/Computação e Algoritmos - Python/Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/Fontes/recursao1.py
+++
@@ -1,9 +0,0 @@
-def recursao(i):
-  print('Recursão')
-  i += 1
-  if i == 5:
-    return
-  else:
-    recursao(i)
-
-recursao(0)
--- Unicatólica/Semestre_2024_1/Computação e Algoritmos - Python/Computação e Algoritmos - Python/Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura+de+Dados+e+Algoritmos+com+Python/Estrutura de Dados e Algoritmos com Python/Fontes/recursao2.py
+++
@@ -1,7 +0,0 @@
-def soma2(n):
-  if n == 0:
-    return 0
-
-  return n + soma2(n - 1)
-
-soma2(5)
