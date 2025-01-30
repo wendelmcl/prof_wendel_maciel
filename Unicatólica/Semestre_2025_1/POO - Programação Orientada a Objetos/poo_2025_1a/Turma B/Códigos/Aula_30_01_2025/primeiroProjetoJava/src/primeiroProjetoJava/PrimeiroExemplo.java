c01c21b6d80f8d4eb62870fd6f7480f66ee2b357
Wendel Maciel <wendelmcl2@gmail.com>
2025-01-30 20:56:18 -0300

    Inserindo novos arquivos no repositório
    Inserindo novos arquivos no repositório

---
+++ Unicatólica/Semestre_2025_1/POO - Programação Orientada a Objetos/poo_2025_1a/Turma B/Códigos/Aula_30_01_2025/primeiroProjetoJava/src/primeiroProjetoJava/PrimeiroExemplo.java
@@ -0,0 +1,17 @@
+package primeiroProjetoJava;
+
+public class PrimeiroExemplo {
+
+	public static void main(String[] args) {
+		int inteiro = 8;
+		double flutuante = 2.6;
+		String palavra = "Sistemas";
+		boolean opcao = true;
+		
+		System.out.println("Inteiro: "+inteiro);
+		System.out.println("Double: "+flutuante);
+		System.out.println("String: "+palavra);
+		System.out.println("Boolean: "+opcao);
+
+	}
+}
---
+++ Unicatólica/Semestre_2025_1/POO - Programação Orientada a Objetos/poo_2025_1a/Turma B/Códigos/Aula_30_01_2025/segundoProjetoJava/src/segundoProjetoJava/SegundaClasse.java
@@ -0,0 +1,17 @@
+package segundoProjetoJava;
+
+public class SegundaClasse {
+
+	public static void main(String[] args) {
+		double valor1 = 100, valor2 = 20;
+		double soma = 0, subtracao = 0, multiplicacao = 0, divisao = 0;
+		soma = valor1 + valor2;
+		subtracao = valor1 - valor2;
+		multiplicacao = valor1 * valor2;
+		divisao = valor1 / valor2;
+		System.out.println("Soma: "+soma);
+		System.out.println("Subtração: "+subtracao);
+		System.out.println("Multiplicação: "+multiplicacao);
+		System.out.println("Divisão: "+divisao);
+	}
+}
