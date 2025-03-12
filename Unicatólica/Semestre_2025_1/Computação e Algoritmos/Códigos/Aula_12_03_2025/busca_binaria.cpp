8ec08c5447f69c31be096f8ce3bfd0e2b663d18e
Wendel Maciel <wendelmcl2@gmail.com>
2025-03-12 20:59:17 -0300

    Aula - 12/03/2025
    Códigos - Aula 12/03/2025

---
+++ Unicatólica/Semestre_2025_1/Computação e Algoritmos/Códigos/Aula_12_03_2025/busca_binaria.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int buscaBinaria(int primos[], int valor){
+    int min = 0, max = 24, mediana = 0;
+    while(min <= max){
+        mediana = (min + max) / 2;
+        if(primos[mediana] == valor){
+            return mediana;
+        }else if(primos[mediana] < valor){
+            min = mediana + 1;
+        }else{
+            max = mediana - 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int primos[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    int valor = 67;
+    int resultado = buscaBinaria(primos, valor);
+    if(resultado != -1){
+        cout << "O número " << valor << " é primo. Encontrado na posicao " << resultado << endl ;
+    }else{
+        cout << "O número não foi encontrado." << endl;
+    }
+
+    return 0;
+}
